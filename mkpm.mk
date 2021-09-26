55f1606e3fb6de8c274a911811d6f08ae7564204
Clay Risser <clayrisser@gmail.com>
2021-09-26 04:28:00 -0500

    Initial bootstrap

--- mkpm.mk
+++ mkpm.mk
@@ -3,7 +3,7 @@
 # File Created: 26-09-2021 00:44:57
 # Author: Clay Risser
 # -----
-# Last Modified: 26-09-2021 01:35:20
+# Last Modified: 26-09-2021 04:27:00
 # Modified By: Clay Risser
 # -----
 # BitSpur Inc (c) Copyright 2021
@@ -24,20 +24,23 @@ MKPM_PACKAGE_DIR := .mkpm
 
 MKPM_PACKAGES := \
 
+NUMPROC := 1
+
 ############# MKPM BOOTSTRAP SCRIPT BEGIN #############
 MKPM_BOOTSTRAP := https://example.com
 NULL := /dev/null
-MKDIRP := mkdir -p
-ifeq ($(SHELL),cmd.exe)
+MKDIR_P := mkdir -p
+ifeq ($(OS),Windows_NT)
+	MKDIR_P = mkdir
 	NULL = nul
-	MKDIRP = mkdir
+	SHELL := cmd.exe
 endif
 -include $(MKPM_PACKAGE_DIR)/bootstrap.mk
 $(MKPM_PACKAGE_DIR)/bootstrap.mk:
-	@$(MKDIRP) $(MKPM_PACKAGE_DIR)
+	@$(MKDIR_P) $(MKPM_PACKAGE_DIR)
 	@cd $(MKPM_PACKAGE_DIR) && \
 		$(shell curl --version >$(NULL) 2>$(NULL) && \
-			echo curl -Ls -o bootstrap.mk|| \
+			echo curl -Ls -o bootstrap.mk || \
 			echo wget -q --content-on-error -O bootstrap.mk) \
 		$(MKPM_BOOTSTRAP) >$(NULL)
 export MKPM := $(shell pwd)/$(MKPM_PACKAGE_DIR)
