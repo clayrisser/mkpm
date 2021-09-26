de34bb8ca8f2005513960806c725605f7fd569b1
Clay Risser <clayrisser@gmail.com>
2021-09-26 18:24:29 -0500

    Updated bootstrap

--- bootstrap.mk
+++ bootstrap.mk
@@ -3,7 +3,7 @@
 # File Created: 26-09-2021 01:25:12
 # Author: Clay Risser
 # -----
-# Last Modified: 26-09-2021 06:29:08
+# Last Modified: 26-09-2021 18:24:02
 # Modified By: Clay Risser
 # -----
 # BitSpur Inc (c) Copyright 2021
@@ -115,7 +115,14 @@ endif
 export NUMPROC ?= $(NPROC)
 export MAKEFLAGS += "-j $(NUMPROC)"
 
-include $(MKPM)/bootstrapping
-$(MKPM)/bootstrapping: $(ROOT)/mkpm.mk
+include $(MKPM)/.bootstrapping
+$(MKPM)/.bootstrapping: $(ROOT)/mkpm.mk
 	@echo ⌛ bootstrapping . . .
+	@$(MAKE) run ARGS="update"
+	@for p in $(MKPM_PACKAGES); do \
+			export PKG=$$(echo $$p | $(SED) 's|=.*$$||g') && \
+			mkdir -p $(MKPM)/.pkgs/$$PKG && \
+			$(MAKE) run ARGS="install $$p --prefix '$(MKPM)/.pkgs/$$PKG'" && \
+			echo "include .pkgs/$$PKG/main.mk" > $(MKPM)/$$PKG; \
+		done
 	@touch -m $@
