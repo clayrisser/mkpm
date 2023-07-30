8e0ba02d3eea54ad4fe37bb57ae81284396cc14c
Clay Risser <clayrisser@gmail.com>
2023-07-30 00:43:58 -0500

    ✨ feat: Update .gitlab-ci.yml and install.sh
    - In .gitlab-ci.yml, updated the script section to include additional file uploads and added a new step to upload the install.sh file.
    - In install.sh, simplified the script by removing unnecessary code and added the ability to specify the MKPM_VERSION.
    - The install.sh script now downloads the mkpm.sh file from the specified GitLab package registry URL using the MKPM_VERSION.
    
    This commit addresses the need to update the CI pipeline configuration and the installation script for the project.

--- install.sh
+++ install.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-_USER_ID=$(id -u $USER)
-_TMP_PATH="${XDG_RUNTIME_DIR:-$([ -d "/run/user/$_USER_ID" ] && \
-    echo "/run/user/$_USER_ID" || echo ${TMP:-${TEMP:-/tmp}})}/mkpm/$$"
+MKPM_VERSION="${MKPM_VERSION:-<% MKPM_VERSION %>}"
 
 sudo true
-mkdir -p $_TMP_PATH
-cd $_TMP_PATH
-curl -LO https://gitlab.com/risserlabs/community/mkpm/-/archive/main/mkpm-main.tar.gz
-tar -xzf mkpm-main.tar.gz
-sudo cp mkpm-main/mkpm.sh /usr/local/bin/mkpm
+sudo curl -L -o /usr/local/bin/mkpm \
+    https://gitlab.com/api/v4/projects/33018371/packages/generic/mkpm/${MKPM_VERSION}/mkpm.sh
 sudo chmod +x /usr/local/bin/mkpm
-rm -rf $_TMP_PATH
