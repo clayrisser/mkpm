91c45e366aad912b4bf02c2733ed1b96dbfa685c
Clay Risser <clayrisser@gmail.com>
2025-03-23 05:47:41 -0500

    Update mkpm-proxy.sh: extract MKPM_VERSION variable for improved URL construction and maintainability.

--- mkpm-proxy.sh
+++ mkpm-proxy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-MKPM_SH_URL="${MKPM_SH_URL:-https://gitlab.com/api/v4/projects/48207162/packages/generic/mkpm/<% MKPM_VERSION %>/mkpm.sh}"
+MKPM_VERSION="<% MKPM_VERSION %>"
+MKPM_SH_URL="${MKPM_SH_URL:-https://gitlab.com/api/v4/projects/48207162/packages/generic/mkpm/${MKPM_VERSION}/mkpm.sh}"
 export PROJECT_ROOT="$(git rev-parse --show-toplevel 2>/dev/null)"
 [ -z "$PROJECT_ROOT" ] || [ ! -f "$PROJECT_ROOT/mkpm.json" ] && exit 1
 if [ ! -f "$PROJECT_ROOT/.mkpm/mkpm/.bin/mkpm" ]; then
